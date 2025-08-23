82397060f00f99007a2af92b389f6afeb5bb0232
Manuel Francisco Haro Arroyo <manuelharo1994@gmail.com>
2025-08-23 12:17:29 -0500

    User model implementation

---
+++ domain/model/src/main/java/co/com/crediya/model/user/User.java
@@ -0,0 +1,24 @@
+package co.com.crediya.model.user;
+import lombok.Builder;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@Builder(toBuilder = true)
+public class User {
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private LocalDate birthDate;
+    private String address;
+    private String email;
+    private String phoneNumber;
+    private String documentNumber;
+    private BigDecimal baseSalary;
+}
