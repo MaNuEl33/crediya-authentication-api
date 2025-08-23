ad376a32bcf725fbef6a892923ec16af40f7cc04
Manuel Francisco Haro Arroyo <manuelharo1994@gmail.com>
2025-08-23 14:01:31 -0500

    Rol model implementation

---
+++ domain/model/src/main/java/co/com/crediya/model/rol/Rol.java
@@ -0,0 +1,15 @@
+package co.com.crediya.model.rol;
+import lombok.Builder;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@Builder(toBuilder = true)
+public class Rol {
+    private Long id;
+    private String name;
+    private String description;
+}
