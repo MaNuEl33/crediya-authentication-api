6ae5a2a22cfe6b3584bee72a55afd8b037439249
Manuel Francisco Haro Arroyo <manuelharo1994@gmail.com>
2025-08-24 22:33:17 -0500

    Add property-naming-strategy jackson configuration

--- infrastructure/entry-points/reactive-web/src/main/java/co/com/crediya/api/dtos/RegisterUserRequestDto.java
+++ infrastructure/entry-points/reactive-web/src/main/java/co/com/crediya/api/dtos/RegisterUserRequestDto.java
@@ -1,16 +1,17 @@
 package co.com.crediya.api.dtos;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.validation.constraints.*;
 import java.math.BigDecimal;
 import java.time.LocalDate;
 
 public record RegisterUserRequestDto(
-        @JsonProperty("first_name") @NotBlank String firstName,
-        @JsonProperty("last_name") @NotBlank String lastName,
-        @JsonProperty("birth_date") LocalDate birthDate, String address,
-        @NotBlank @Email String email, @JsonProperty("phone_number") String phoneNumber,
-        @JsonProperty("document_number") String documentNumber,
-        @JsonProperty("base_salary") @NotNull @DecimalMin("0") @DecimalMax("15000000") BigDecimal baseSalary,
-        @JsonProperty("role_id") @NotNull Long roleId) {
+        @NotBlank String firstName,
+        @NotBlank String lastName,
+        LocalDate birthDate,
+        String address,
+        @NotBlank @Email String email,
+        String phoneNumber,
+        String documentNumber,
+        @NotNull @DecimalMin("0") @DecimalMax("15000000") BigDecimal baseSalary,
+        @NotNull Long roleId) {
 }
--- infrastructure/entry-points/reactive-web/src/main/java/co/com/crediya/api/dtos/RegisterUserResponseDto.java
+++ infrastructure/entry-points/reactive-web/src/main/java/co/com/crediya/api/dtos/RegisterUserResponseDto.java
@@ -1,16 +1,15 @@
 package co.com.crediya.api.dtos;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
 import java.math.BigDecimal;
 import java.time.LocalDate;
 
 public record RegisterUserResponseDto(
         Long id,
-        @JsonProperty("first_name") String firstName,
-        @JsonProperty("last_name") String lastName,
-        @JsonProperty("birth_date") LocalDate birthDate, String address, String email,
-        @JsonProperty("phone_number") String phoneNumber,
-        @JsonProperty("document_number") String documentNumber,
-        @JsonProperty("base_salary") BigDecimal baseSalary,
-        @JsonProperty("role_id") Long roleId) {
+        String firstName,
+        String lastName,
+        LocalDate birthDate, String address, String email,
+        String phoneNumber,
+        String documentNumber,
+        BigDecimal baseSalary,
+        Long roleId) {
 }
